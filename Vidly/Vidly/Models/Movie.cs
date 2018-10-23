765122a431c6e525bfa4e335f6bcf7e94292f9bf
DESKTOP-5RABSME\Richard <rkwong792@gmail.com>
2018-10-23 00:12:37 -0400

    Movies/Random page created

---
+++ Vidly/Vidly/Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Models
+{
+    //POCO - Plain Old CRL Object
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
